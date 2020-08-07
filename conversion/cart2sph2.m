73494e2dfd4413e0bbb4223dcff9fd533a4fb9eb
Ido Hadi <idohadi@mail.tau.ac.il>
2020-08-07 23:21:52 +0300

    documented convention

--- conversion/cart2sph2.m
+++ conversion/cart2sph2.m
@@ -1,3 +1,39 @@
 function [theta, phi, rho] = cart2sph2(x, y, z)
-% TODO: convert 3d vector [x(j), y(j), z(j)] into spherical coordinates
-% Use convention such that theta in [0,pi], phi in [0,2pi] and rho>=0.
+%%
+% Convert a vector in R^3 from Cartesian coordinates to spherical 
+% coordinates.
+% 
+% Convention
+%   Variable    Interval
+%   theta       [0,pi]
+%   phi         [0,2pi)
+%   rho         [0,infinity)
+% 
+% theta, phi, rho satisfy
+%   x(j) = rho*cos(phi)*sin(theta)
+%   y(j) = rho*sin(phi)*sin(theta)
+%   z(j) = rho*cos(theta)
+% 
+% Input arguments
+%   x   double      x coordinates of the vector.
+%   y   double      y coordiantes of the vector.
+%   z   double      z coordiantes of the vector.
+%   x, y, z must have the same dimensions.
+% 
+% Output arguments
+%   theta       See convention.
+%   phi         [0,2pi)
+%   rho         [0,infinity)
+% 
+% Notes
+%   This function performs no input checks.
+% 
+% Reference
+%   None
+% ***********************************************************
+% Author    Ido Hadi
+% Email     idohadi@mail.tau.ac.il
+% Year      2020
+% ***********************************************************
+%% Cartesian->Spherical coordiantes
+% TODO
--- conversion/sph2cart2.m
+++ conversion/sph2cart2.m
@@ -1,3 +1,39 @@
 function [x, y, z] = sph2cart2(theta, phi, rho)
-% TODO: convert 3d vector from spherical coordinates into [x(j), y(j), z(j)] 
-% Use convention such that theta in [0,pi], phi in [0,2pi] and rho>=0.
+%%
+% Convert a vector in R^3 from spherical coordinates to Cartesian 
+% coordinates.
+% 
+% Convention
+%   Variable    Interval
+%   theta       [0,pi]
+%   phi         [0,2pi)
+%   rho         [0,infinity)
+% 
+% theta, phi, rho satisfy
+%   x(j) = rho*cos(phi)*sin(theta)
+%   y(j) = rho*sin(phi)*sin(theta)
+%   z(j) = rho*cos(theta)
+% 
+% Input arguments
+%   theta       See convention.
+%   phi         [0,2pi)
+%   rho         [0,infinity)
+% 
+% Output arguments
+%   x   double      x coordinates of the vector.
+%   y   double      y coordiantes of the vector.
+%   z   double      z coordiantes of the vector.
+%   x, y, z must have the same dimensions.
+% 
+% Notes
+%   This function performs no input checks.
+% 
+% Reference
+%   None
+% ***********************************************************
+% Author    Ido Hadi
+% Email     idohadi@mail.tau.ac.il
+% Year      2020
+% ***********************************************************
+%% Spherical coordiantes->Cartesian
+% TODO
