1e97abb69a03b6fdb5d1b1f7fabd01121107f2e9
Ido Hadi <idohadi@mail.tau.ac.il>
2020-08-07 15:46:36 +0300

    changed the folder structure

--- mex/compile_mex.m
+++
@@ -1,9 +0,0 @@
-%% Compiling all th mex functions
-mex -R2018a COMPFLAGS='$COMPFLAGS -Ofast -msse2 -DHAVE_SSE2' -DSFMT_MEXP=2281 applyRotation.c ..\c\utility_functions.c ..\c\healpix_functions.c ..\c\rotation_grid.c ..\extern\SFMT-1.5.1\SFMT.c
-mex -R2018a COMPFLAGS='$COMPFLAGS -Ofast' calculateRotationDistance.c ..\c\utility_functions.c ..\c\healpix_functions.c ..\c\rotation_grid.c
-mex -R2018a COMPFLAGS='$COMPFLAGS -Ofast -msse2 -DHAVE_SSE2' -DSFMT_MEXP=2281 estimateDispersionOfRotationsSet.c ..\c\utility_functions.c ..\c\healpix_functions.c ..\c\rotation_grid.c ..\extern\SFMT-1.5.1\SFMT.c
-mex -R2018a COMPFLAGS='$COMPFLAGS -Ofast -msse2 -DHAVE_SSE2' -DSFMT_MEXP=2281 estimateDispersionOfUniformRotationsGrid.c ..\c\utility_functions.c ..\c\healpix_functions.c ..\c\rotation_grid.c ..\extern\SFMT-1.5.1\SFMT.c
-mex -R2018a COMPFLAGS='$COMPFLAGS -Ofast -msse2 -DHAVE_SSE2' -DSFMT_MEXP=2281 generateUniformlyRandomRotations.c ..\c\utility_functions.c ..\c\healpix_functions.c ..\c\rotation_grid.c ..\extern\SFMT-1.5.1\SFMT.c
-mex -R2018a COMPFLAGS='$COMPFLAGS -Ofast -msse2 -DHAVE_SSE2' -DSFMT_MEXP=2281 generateUniformRotationSequencePoint.c ..\c\utility_functions.c ..\c\healpix_functions.c ..\c\rotation_grid.c ..\extern\SFMT-1.5.1\SFMT.c
-mex -R2018a COMPFLAGS='$COMPFLAGS -Ofast' generateUniformRotationsGridInHopf.c ..\c\utility_functions.c ..\c\healpix_functions.c ..\c\rotation_grid.c
-mex -R2018a COMPFLAGS='$COMPFLAGS -Ofast' generateUniformRotationsGridInQuaternions.c ..\c\utility_functions.c ..\c\healpix_functions.c ..\c\rotation_grid.c
