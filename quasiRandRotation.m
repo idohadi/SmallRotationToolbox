de7b34d8cc86bba6d4efdddb573624569eef37b9
Ido Hadi <idohadi@mail.tau.ac.il>
2020-08-07 15:56:40 +0300

    y

--- quasiRandRotation.m
+++ quasiRandRotation.m
@@ -1,3 +1,4 @@
-function rotation = quasiRandRotation(n)
+function rotation = quasiRandRotation(n, varargin)
 % TODO: returns the nth (n>=1) rotation from the rotation grid
 % Can return in quaternion (default) or in hopf 
+% n can be a list 
