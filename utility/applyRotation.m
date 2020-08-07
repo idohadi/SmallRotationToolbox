888bfe7f17d51b26ce21151326138bf115b3f701
Ido Hadi <idohadi@mail.tau.ac.il>
2020-08-07 16:25:12 +0300

    implemented function applying a rotation; needs to be tested

---
+++ utility/applyRotation.m
@@ -0,0 +1,48 @@
+function xRot = applyRotation(rotation, x)
+%%
+% Apply the rotation to vectors in R^3.
+% 
+% Input arguments
+%   rotation    double      4 x M real array, such that rotation(:, m) is a
+%                           unit quaternion representing a rotation.
+%   x           double      3 x N real array.
+% 
+% Output arguments
+%   xRot        double      3 x N x M real array, 
+%                               xRot(:, n, m) = rotation(:, m)*x(:, n).
+% 
+% Notes
+%   This is a wrapper of the mex function applyRotation_mex.
+% 
+% Reference
+%   None
+% ***********************************************************
+% Author    Ido Hadi
+% Email     idohadi@mail.tau.ac.il
+% Year      2020
+% ***********************************************************
+%% Input validation
+assert(ismatrix(x), 'x must be a matrix.');
+if isempty(x)
+    xRot = [];
+    warning('x is empty.');
+    return;
+end
+assert(size(x, 1)==3, 'x must have 3 rows.'); 
+
+if isempty(rotation)
+    xRot = x;
+    warning('Rotation is empty.');
+    return;
+end
+assert(size(rotation, 1)==4, ...
+    ['Rotation must be a unit quaternion. ', ...
+    'In particular, it must have 4 rows.']);
+
+%% Calling the mex function
+xRot = cell2mat(...
+    cellfun(...
+        @(r) applyRotation_mex(r, x), num2cell(rotation) ...
+        ) ...
+    );
+xRot = reshape(xRot, [size(x), size(rotation, 2)]);
