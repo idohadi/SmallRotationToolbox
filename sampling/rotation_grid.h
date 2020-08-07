61168f972dfb216c4e4ae5bea8c40c2ba8bcb54e
Ido Hadi <idohadi@mail.tau.ac.il>
2020-08-07 20:57:17 +0300

    deleted approximate_incremental_rotation_grid_dispersion

--- sampling/rotation_grid.h
+++ sampling/rotation_grid.h
@@ -11,10 +11,3 @@ void initialize_incremental_rotation_grid(double *initial_rotation_grid);
 void generate_incremental_rotation_grid_point(size_t n, double *initial_rotation_grid, double *rotation);
 
 void generate_incremental_rotation_grid_point_recur(size_t base_grid_index, size_t current_index, size_t resolution_level, size_t healpix_point, double psi_in_degrees, double *rotation_in_hopf);
-
-// void approximate_incremental_rotation_grid_dispersion(size_t sampled_rotations_no, 
-//                                                         size_t sequence_lower_bound, 
-//                                                         size_t sequence_upper_bound, 
-//                                                         double *initial_rotation_grid, 
-//                                                         sfmt_t *sfmt, 
-//                                                         double *dispersion);
