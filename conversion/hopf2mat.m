0394ef115f01fe544fa03e00ff6737861155f42a
Ido Hadi <idohadi@mail.tau.ac.il>
2020-08-08 03:03:10 +0300

    deleted two functions I gave up on

--- conversion/hopf2mat.m
+++
@@ -1,2 +0,0 @@
-function mat = hopf2mat(hopf)
-% TODO: convert rotations from hopf coordinates to rotation matrix
--- conversion/mat2hopf.m
+++
@@ -1,2 +0,0 @@
-function hopf = mat2hopf(mat)
-% TODO: convert rotations from rotation matrix coordinates to hopf
